e3ce69c06d597eb6cf86dfb26df36f5f180f6686
ahmed9088 <memon1ahmed@gmail.com>
2025-01-24 12:29:18 +0500

    Updated Node JS Api fecting

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const express = require('express'); // Import express
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port =8000
+const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    res.send('GET request received');
+});
 
-app.get('/',(req,res)=>{
-    console.log('get response')
-    res.send('request received')
-})
+app.get('/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'Corolla', price: 100 },
+        { id: 2, name: 'Civic', price: 200 },
+        { id: 3, name: 'WagonR', price: 300 }
+    ];
+    res.send({ message: 'Products fetched successfully', data: products });
+});
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send({ message: 'POST request received', data: req.body });
+});
 
-app.post('/',(req,res)=>{
-    console.log(req.body)
-    res.send(req.body)
+app.put('/', (req, res) => {
+    console.log(req.body);
+    res.send({ message: 'PUT request received', data: req.body });
+});
 
-})
+app.patch('/', (req, res) => {
+    console.log(req.body);
+    res.send({ message: 'PATCH request received', data: req.body });
+});
 
-
-// app.get('/',(req,res)=>{
-//     console.log('call Get apis')
-//     res.send('heelo')
-// })0
-
-
-app.listen(port,()=>{
-console.log('server work',port)
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
